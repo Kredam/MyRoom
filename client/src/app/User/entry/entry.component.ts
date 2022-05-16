565243363441adee007e755f318a95eef8ce55f2
Kredam <akitlosz2000@gmail.com>
2022-05-16 23:50:24 +0200

    entry in flex

--- client/src/app/User/entry/entry.component.ts
+++ client/src/app/User/entry/entry.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
-  styleUrls: []
+  styleUrls: ['./entry.component.scss'],
+  animations: [
+    trigger('onEnterDOM', [
+      transition(':enter', [
+        style({opacity:0}),
+        animate('1s ease-in-out'),
+        style({opacity:1})
+      ])
+    ])
+  ]
 })
 export class EntryComponent implements OnInit {
+  isRegisterOnDisplay: boolean;
 
-  constructor() { }
+  constructor() { 
+    this.isRegisterOnDisplay = true
+  }
+  changeIsRegisterOn(state : boolean){
+    this.isRegisterOnDisplay = state
+  }
 
   ngOnInit(): void {
   }
