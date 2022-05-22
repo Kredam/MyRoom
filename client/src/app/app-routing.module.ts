e33431dd9687456a088341fb46e526e01f82031a
Kredam <akitlosz2000@gmail.com>
2022-05-22 14:38:32 +0200

    add dynamic(room) urls

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ArticleComponent } from './room/article/article.component';
+import { RoomComponent } from './room/room.component';
 import { EntryComponent } from './User/entry/entry.component';
+import { ProfileComponent } from './User/profile/profile.component';
 
 
 const routes: Routes = [
   { path: 'signin', component:EntryComponent },
   { path: '' , component: HomeComponent },
+  { path: 'room/:room', component: RoomComponent, 
+    children:[
+      {path:"article/:article", component:ArticleComponent}
+    ]
+  },
+  { path:"user/:uid", component: ProfileComponent},
   { path: '**', component: HomeComponent }
 ];
 
