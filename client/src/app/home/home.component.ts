0142ff7847283a7f48666cc849f437716799f4a4
Kredam <akitlosz2000@gmail.com>
2022-05-22 14:39:15 +0200

    angular material cards

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { RoomService } from '../services/rooms.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: []
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
