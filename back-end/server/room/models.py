19d532090c89b809957a79d43cf553338e0513e8
Kredam <akitlosz2000@gmail.com>
2022-06-15 22:48:44 +0200

    removed article related models

--- back-end/server/room/models.py
+++ back-end/server/room/models.py
@@ -17,21 +17,6 @@ class Topics(models.Model):
   # room should be primary key
   room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
 
-class Article(models.Model):
-  room = models.ForeignKey(Room, on_delete=models.CASCADE)
-  title = models.CharField(max_length=100)
-  likes = models.IntegerField(default=0)
-  body = models.TextField(blank=True)
-
-# many to one
-class Comments(models.Model):
-  room = models.ForeignKey(Room, on_delete=models.CASCADE)
-  user = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True)
-  body = models.TextField(blank=True)
-  likes = models.IntegerField(default=0)
-  create = models.DateTimeField(auto_now_add=True)
-  update = models.DateTimeField(auto_now=True)
-
 class Followed(models.Model):
   room = models.ForeignKey(Room, on_delete=models.CASCADE)
   user = models.ForeignKey('users.User', on_delete=models.CASCADE)
