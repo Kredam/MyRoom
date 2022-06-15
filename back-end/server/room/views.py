c9340082f927e5ea8505823eb444d2b8f7131e05
Kredam <akitlosz2000@gmail.com>
2022-06-15 22:40:52 +0200

    Implemented follow rooms action

--- back-end/server/room/views.py
+++ back-end/server/room/views.py
@@ -1,34 +1,50 @@
-from django.shortcuts import get_list_or_404
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
-from rest_framework.viewsets import ModelViewSet, ViewSet
-
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from .models import Article, Room, Followed
-from .serializer import ArticleSerializer, RoomSerializer, FollowedSerializer
+from .models import Room, Followed
+from article.models import Article
+from .serializer import RoomSerializer, FollowedSerializer
+from article.serializer import ArticleSerializer
 
 class GetAllRoomsView(ListAPIView):
   serializer_class = RoomSerializer
   queryset = Room.objects.all()[:5]
 
-class ArticleTestView(ListAPIView):
-  serializer_class = RoomSerializer
-
-  def get_queryset(self):
-      return get_list_or_404(Article, room=self.kwargs.get('pk'))
 
 class ArticlesViewSet(ModelViewSet):
   serializer_class = ArticleSerializer
   queryset = Article.objects.all()
 
+  def get_object(self):
+    pk = self.kwargs["pk"]
+    return get_object_or_404(Article, id=pk)
 
   def get_queryset(self):
     room = self.kwargs.get('pk')
     return get_list_or_404(Article, room=room)
 
+  @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+  def follow(self, request, pk=None):
+    room = Room.objects.get(name=request.data['name'])
+    admin = request.data['isAdmin']
+    serializer = FollowedSerializer(data={'room':room, 'user':request.user.pk, 'isAdmin':admin})
+    print(serializer.initial_data)
+    if serializer.is_valid():
+      print(serializer.validated_data)
+      serializer.save()
+      return Response('Successfull follow')
+    print(serializer.errors)
+    return Response("Not Successfull")
 
 
+class RoomViewSet(ModelViewSet):
+  queryset = Room.objects.all()
+  serializer_class = FollowedSerializer
+
+    
 class ListFollowedRooms(ListAPIView):
   permission_classes = [IsAuthenticated]
   serializer_class = RoomSerializer
