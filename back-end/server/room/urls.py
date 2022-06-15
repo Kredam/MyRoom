7862ab154ebb37b08f8c81061efb7ae89376912f
Kredam <akitlosz2000@gmail.com>
2022-06-15 22:55:12 +0200

    added follow path

--- back-end/server/room/urls.py
+++ back-end/server/room/urls.py
@@ -1,15 +1,17 @@
+from django import urls
 from django.urls import include, path
-from .views import GetAllRoomsView, ListFollowedRooms, ArticlesViewSet, ArticleTestView
+from .views import GetAllRoomsView, ListFollowedRooms, ArticlesViewSet, RoomViewSet
 from rest_framework import routers
 
 article = routers.DefaultRouter()
 article.register('', ArticlesViewSet, basename='article')
+article.register('article', ArticlesViewSet)
+# article.register('article', ArticlesViewSet, basename='article')
 
 urlpatterns = [
   # name = easy to reference in view template
   path('all/', GetAllRoomsView.as_view()),
+  path('follow/', ArticlesViewSet.as_view({'post' : 'follow'})),
   path('followed-rooms/', ListFollowedRooms.as_view()),
-  path('<str:pk>/', include(article.urls))
-  # path('<str:pk>/', ArticleTestView.as_view()),
+  path('<str:pk>/', include(article.urls)),
 ]
-
