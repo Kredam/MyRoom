5e79c9e423b5e8a3c28d26aef85f05615a0b2ad7
Kredam <akitlosz2000@gmail.com>
2022-06-17 23:36:51 +0200

    model rewrite for follow

--- client/src/app/model/room.ts
+++ client/src/app/model/room.ts
@@ -1,5 +1,9 @@
+import { Observable } from "rxjs"
+import { IFollowed } from "./followed"
+
 export interface IRoom{
   name: string,
   description?: string,
   picture?: string
+  isFollowed?: Observable<Boolean>
 }
