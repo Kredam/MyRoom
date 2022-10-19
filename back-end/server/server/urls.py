9eca3c1f2bd6225ec7dea67a11d9063a83ca7cf8
Kredam <akitlosz2000@gmail.com>
2022-10-19 19:20:43 +0200

    custom login to add refresh token to cookies

--- back-end/server/server/urls.py
+++ back-end/server/server/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/rooms/', include('room.urls')),
-    path('api/token', include('api.urls')),
+    path('api/token/', include('api.urls')),
     path('api/article/', include('article.urls'))
 ]
  
