79cead120a51c07bab41b850c7e51bc45d04f52d
Kredam <akitlosz2000@gmail.com>
2022-12-19 18:36:37 +0100

    added allowed host and cors allow changed

--- back-end/server/server/settings.py
+++ back-end/server/server/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'users.User'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://192.168.0.31:3000'
+    "http://localhost:3001"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -85,10 +84,19 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        # "HOST": "db",
+        "PORT": 5432
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 REST_FRAMEWORK = {
@@ -99,6 +107,10 @@ REST_FRAMEWORK = {
     )
     
 }
+
+ALLOWED_HOSTS = [
+    "*"
+]
 
 # Django project settings.py
 
